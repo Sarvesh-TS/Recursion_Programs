7be0bc6f8413791cf8754110376bd1d3c0dd1491
Sarvesh T S <128053748+Sarvesh-TS@users.noreply.github.com>
2025-07-16 08:59:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
-class main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello my dear zomato");
+//Print Hello my dear N times using Recursion .
+public class Main{
+    public static void printMessage(int n){
+        if(n==0){ //base case
+            return;
+        }
+            System.out.println("Hello my dear");
+            printMessage(n-1); //recursive call itself
+        }
+    public static void main(String[] args){
+        int n =5;
+        printMessage(n);
+        
     }
 }
+
